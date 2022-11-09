45547b3b05136499f248d78c5666dd26e9c5844e
chuk1123 <chuk1123@icloud.com>
2022-11-08 18:51:20 -0800

    update about pages

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,15 @@
 <template>
 <v-container class="py-10 mb-6">
   <div class="text-h3 deep-purple--text text-center font-weight-bold mb-10">About Me</div>
+  <br>
   <v-row class="justify-center">
     <v-col cols=8>
       <div class="text-h6">
         {{bio}}
       </div>
     </v-col>
-    <v-col cols=4>
+    <v-col cols=3>
+      <v-img src="Python-logo.png"></v-img>
     </v-col>
   </v-row>
 </v-container>
@@ -19,7 +21,7 @@ export default {
         return {
           bio: `Hello, my name is Kevin. 
                I am a student at Northwood High School in Irvine, California. I am interested in computer science and codes in Python, C++ and Javascript. 
-               I have experience in machine learning, discord bots, web development, and competitive programming."
+               I have experience in machine learning, discord bots, web development, and competitive programming.
                `
         }
     },
