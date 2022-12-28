66b062c5e8da7caa6a9d33bfd60f83d6f86d38c3
chuk1123 <chuk1123@icloud.com>
2022-12-27 22:58:45 -0800

    update github footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -25,6 +25,10 @@
       <v-container class="fill-height" style="max-width: 1280px">
         <span class="ml-6">&copy; {{ new Date().getFullYear() }} Chuk</span>
         <v-spacer></v-spacer>
+        <v-btn medium class="mx-10 elevation-0" color="transparent" href="https://github.com/chuk1123/chuk-portfolio" target="_blank">
+            <v-icon class="mr-2" size="30">mdi-github</v-icon>
+            Website Github
+        </v-btn>
         <span class="mr-6">Personal Portfolio</span>
       </v-container>
     </v-footer>
