eab15f6a7e1beca4c8018f86eb7afc559218090d
chuk1123 <chuk1123@icloud.com>
2023-01-27 21:04:38 -0800

    update achievements

--- pages/index.vue
+++ pages/index.vue
@@ -1,71 +1,111 @@
 <template>
-<div>
-  <v-container class="py-16">
-    <v-row class="align-center justify-center">
-      <v-col cols=12 md=6>
-        <div class="text-h5 mb-6">Hello, my name is </div>
-        <div class="display-3 mb-6"><span class="deep-purple--text font-weight-medium">Kevin Chu</span></div>
-        <div class="text-h5 mb-6">Code in Python, C++, Javascript</div>
-        <div class="text-h6 mb-6">Machine Learning, Web Development, Discord Bots</div>
-        <br>
-        <v-btn large class="px-3 ma-2 white--text" color="red" href="https://www.youtube.com/channel/UCwPzHYyJy85mEExyrVLF2Hw" target="_blank">
+  <div>
+    <v-container class="py-16">
+      <v-row class="align-center justify-center">
+        <v-col cols="12" md="6">
+          <div class="text-h5 mb-6">Hello, my name is</div>
+          <div class="display-3 mb-6">
+            <span class="deep-purple--text font-weight-medium">Kevin Chu</span>
+          </div>
+          <div class="text-h5 mb-6">Code in Python, C++, Javascript</div>
+          <div class="text-h6 mb-6">
+            Machine Learning, Web Development, Discord Bots
+          </div>
+          <br />
+          <v-btn
+            large
+            class="px-3 ma-2 white--text"
+            color="red"
+            href="https://www.youtube.com/channel/UCwPzHYyJy85mEExyrVLF2Hw"
+            target="_blank"
+          >
             <v-icon left size="16">mdi-youtube</v-icon>
             Youtube
-        </v-btn>
-        <v-btn large class="px-3 ma-2 white--text" color="pink" href="https://www.instagram.com/chuk1123/" target="_blank">
+          </v-btn>
+          <v-btn
+            large
+            class="px-3 ma-2 white--text"
+            color="pink"
+            href="https://www.instagram.com/chuk1123/"
+            target="_blank"
+          >
             <v-icon left size="16">mdi-instagram</v-icon>
             Instagram
-        </v-btn>
-        <v-btn large class="px-3 ma-2" color="info" href="https://www.linkedin.com/in/kevin-chu-072b7a211/" target="_blank">
+          </v-btn>
+          <v-btn
+            large
+            class="px-3 ma-2"
+            color="info"
+            href="https://www.linkedin.com/in/kevin-chu-072b7a211/"
+            target="_blank"
+          >
             <v-icon left size="16">mdi-linkedin</v-icon>
             Linkedin
-        </v-btn>
-        <v-btn large class="px-3 ma-2" color="dark" href="https://github.com/chuk1123" target="_blank">
+          </v-btn>
+          <v-btn
+            large
+            class="px-3 ma-2"
+            color="dark"
+            href="https://github.com/chuk1123"
+            target="_blank"
+          >
             <v-icon left size="16">mdi-github</v-icon>
             Github
-        </v-btn>
-      </v-col>    
-      <v-col cols=12 md=4>
-        <v-img class="rounded-xl elevation-20 mx-auto" max-height="400" max-width="400" src="kevin-photo.jpg"/>
-      </v-col>
-    </v-row>
-  </v-container>
-  <v-divider></v-divider>
-  <v-container class="py-16">
-    <div class="text-h3 deep-purple--text text-center font-weight-bold mb-10">
-      About Me
-    </div>
-    <h7 class="double-space mx-md-16 mb-6 text-h6"> 
-      {{bio}} 
-    </h7>
-    <nuxt-link 
-      class="text-decoration-none font-weight-bold text-h6 deep-purple--text mx-md-16" 
-      to="/about"
-    >
-      Learn More
-    </nuxt-link>
-  </v-container>
-  <v-divider></v-divider>
-  <v-container class="py-16">
-    <div class="text-h3 deep-purple--text text-center font-weight-bold mb-10">Projects</div>
-    <v-row class="my-10" justify="center">
-      <v-col v-for="item in projects" :key="item.id" cols=12 md=6>
-        <v-card class="pa-6 rounded-xl fill-height">
-          <v-card-title>
-            {{item.title}}
-          </v-card-title>
-          <v-card-subtitle>
-            {{item.subtitle}}
-          </v-card-subtitle >
-          <div v-for="image in item.image" :key=image.id >
-            <v-img :src="image" class="mt-6 mx-auto rounded-xl" max-height="250" max-width="400" contain/>            
-          </div>
-        </v-card>
-      </v-col>
-    </v-row>
-  </v-container>
-    
-</div>
+          </v-btn>
+        </v-col>
+        <v-col cols="12" md="4">
+          <v-img
+            class="rounded-xl elevation-20 mx-auto"
+            max-height="400"
+            max-width="400"
+            src="kevin-photo.jpg"
+          />
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-divider></v-divider>
+    <v-container class="py-16">
+      <div class="text-h3 deep-purple--text text-center font-weight-bold mb-10">
+        About Me
+      </div>
+      <h7 class="double-space mx-md-16 mb-6 text-h6">
+        {{ bio }}
+      </h7>
+      <nuxt-link
+        class="text-decoration-none font-weight-bold text-h6 deep-purple--text mx-md-16"
+        to="/about"
+      >
+        Learn More
+      </nuxt-link>
+    </v-container>
+    <v-divider></v-divider>
+    <v-container class="py-16">
+      <div class="text-h3 deep-purple--text text-center font-weight-bold mb-10">
+        Projects
+      </div>
+      <v-row class="my-10" justify="center">
+        <v-col v-for="item in projects" :key="item.id" cols="12" md="6">
+          <v-card class="pa-6 rounded-xl fill-height">
+            <v-card-title>
+              {{ item.title }}
+            </v-card-title>
+            <v-card-subtitle>
+              {{ item.subtitle }}
+            </v-card-subtitle>
+            <div v-for="image in item.image" :key="image.id">
+              <v-img
+                :src="image"
+                class="mt-6 mx-auto rounded-xl"
+                max-height="250"
+                max-width="400"
+                contain
+              />
+            </div>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
@@ -90,18 +130,18 @@ export default {
           subtitle: "AI to Play Snake Using Deep Reinforcement Learning",
           image: [
             "https://media.istockphoto.com/vectors/classic-retro-snakestyle-game-basic-digital-screen-for-your-design-vector-id1363586427?k=20&m=1363586427&s=612x612&w=0&h=uKW6cbMQCS4ydTzsdIaRiUc1L7Ike-3N0qzBjm-uwis=",
-            "https://cdn-icons-png.flaticon.com/512/6461/6461928.png"
-          ]
+            "https://cdn-icons-png.flaticon.com/512/6461/6461928.png",
+          ],
         },
-      ]
-    }
+      ],
+    };
   },
   head() {
     return {
-      title: 'Home'
-    }
-  }
-}
+      title: "Home",
+    };
+  },
+};
 </script>
 
 <style>
